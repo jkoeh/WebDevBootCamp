5c218f4f0c5fcaf816d750fc929d47a2f49e2f01
jkoeh <johnkoeh@gmail.com>
2018-11-29 09:21:41 -0800

    frontend

--- fullstack_todo_app/frontend/src/App.js
+++ fullstack_todo_app/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TodoList from './TodoList';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Hi</h1>
+         <TodoList/>
         </header>
       </div>
     );
