97eca9a02261c9bcdaa942034b6b474faad383fa
jkoeh <johnkoeh@gmail.com>
2018-12-08 11:46:04 -0800

    fix store bug

--- react-redux-todos/src/index.js
+++ react-redux-todos/src/index.js
@@ -8,7 +8,7 @@ import rootReducer from './reducers/rootReducer'
 import { Provider } from 'react-redux';
 const store = createStore(
     rootReducer,
-    +   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 ReactDOM.render(<Provider store={store}>
     <App />
