5f5d447cd083bc56b73f842de16cc6ee518cfbfc
Koeh, Johann <jkoeh@illumina.com>
2018-12-25 17:26:30 -0800

    backend finished

--- warbler/server/index.js
+++ warbler/server/index.js
@@ -5,20 +5,38 @@ const cors = require('cors');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 const app = express();
-const errorHandler = require('./handlers/error') 
+const errorHandler = require('./handlers/error')
 const authRoutes = require('./routes/auth');
+const messageRoute = require('./routes/message');
+const { loginRequired, ensureCorrectUser } = require('./middleware/auth');
+const db = require('./models');
 app.use(cors());
 app.use(bodyParser.json())
 
 app.use('/api/auth', authRoutes);
-
-app.use(function(req, res, next){
+app.use('/api/users/:id/messages', loginRequired, ensureCorrectUser, messageRoute);
+app.get('/api/messages', loginRequired, async function (req, res, next) {
+  try {
+    let messages = await db.Message.find()
+      .sort({ creatAt: 'desc' })
+      .populate('user', {
+        username: true,
+        profileImageUrl: true
+      });
+      return res.status(200).json(messages);
+  }
+  catch (e) {
+    next(e);
+  }
+});
+app.use(function (req, res, next) {
   let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
+
 app.use(errorHandler);
 
-app.listen(PORT, HOST, function(){
+app.listen(PORT, HOST, function () {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
--- warbler/server/models/message.js
+++ warbler/server/models/message.js
@@ -11,6 +11,8 @@ const messageSchema = new Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     }
+}, {
+    timestamps: true
 });
 messageSchema.pre('remove', async function (next) {
     try {
