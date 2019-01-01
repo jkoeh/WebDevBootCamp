b11c17249a32f150c6a6912f22154762d34c8392
Koeh, Johann <jkoeh@illumina.com>
2018-12-31 21:55:15 -0800

    hi

---
+++ warbler/client/src/containers/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Logo from '../images/warbler-logo.png';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <Link to="/" className="navbar-brand">
+                        <img src={Logo} alt="Warbler Home"></img>
+                    </Link>
+                    <ul className="nav navbar-nav navbar-right">
+                        <li>
+                            <Link to="/signup">Sign Up</Link>
+                        </li>
+                        <li>
+                            <Link to="/signin">Log In</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+function mapStateToProps(state) {
+    return {
+        currentUser: state.currentUser
+    };
+}
+export default connect(mapStateToProps, null)(Navbar);
