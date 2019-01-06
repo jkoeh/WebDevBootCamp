524d5fa20cf9ef804916da852c044adfed1ca44e
Koeh <johnkoeh@gmail.com>
2019-01-06 15:37:26 -0800

    log out

---
+++ warbler/client/src/components/MessageTimeline.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MessageList from './../containers/MessageList';
+const MessageTimeline = props => {
+    return (
+        <div>
+            <MessageList />
+        </div>
+    );
+};
+
+export default MessageTimeline;
